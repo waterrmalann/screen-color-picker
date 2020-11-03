81b8e25ca6573c3c03a603ce52a61ec9e2d8912b
Alan Biju <=>
2020-11-03 18:36:42 +0800

    better documentation of code

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A python application that enables you to pick colors from your screen using your mouse cursor to point.
 
+## Usage
+
+Keep the program running in the background, move your mouse cursor to any point on the screen. Hit the keyboard shortcut combination (CTRL + ALT + C by default). The hex color code of the pixel on that exact point your cursor was on will be sent to your clipboard (eg: #ffffff). It'll also get printed on the console application alongside it's RGB value and the coordinate.
+
 ## Requirements
 
 - [Python](https://www.python.org/) 3.7 or above.
--- color_picker.py
+++ color_picker.py
@@ -11,48 +11,61 @@
 	-> It also shows up on the console application alongside it's RGB value.
 """
 
-import keyboard
-import pyscreenshot
-import pyperclip
-import ctypes
-import os
+import keyboard  # to set up and listen to hotkeys.
+import pyscreenshot  # to take a screenshot. (duh)
+import pyperclip  # to copy color codes to clipboard.
+import ctypes  # to access cursor position.
+import os  # to clear screen and set window title.
+
+HOTKEY = "ctrl+alt+c"
 
 try:
 	# https://stackoverflow.com/a/32541666/11912727
+	# for accurate cursor coordinates.
 	ctypes.windll.user32.SetProcessDPIAware()
 except AttributeError:
-	pass
+	pass  # apparently it doesn't work on Windows XP.
 	
 class POINT(ctypes.Structure):
 	_fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
 
 def get_position():
+	"""Get current cursor position."""
+	
 	cursor = POINT()
+	# grab the position of the mouse cursor.
 	ctypes.windll.user32.GetCursorPos(ctypes.byref(cursor))
 	return (cursor.x, cursor.y)
 
-clamp = lambda x: max(0, min(x, 255))
 def rgb2hex(rgb):
+	"""To convert the RGB values to HEX."""
+	
+	clamp = lambda x: max(0, min(x, 255))
 	return f"#{clamp(rgb[0]):02x}{clamp(rgb[1]):02x}{clamp(rgb[2]):02x}"
 	
 def get_color(coordX, coordY):
-	screenie = pyscreenshot.grab()
+	"""To get the color of the pixel of a given coord on the screen."""
+	
+	screenie = pyscreenshot.grab()  # take a screenshot.
 	image = screenie.convert('RGB')
-	r, g, b = image.getpixel((coordX, coordY))
+	r, g, b = image.getpixel((coordX, coordY))  # get the color of the pixel at the coords.
 	return (r, g, b)
 
 def on_hotkey_press():
+	"""Called when CTRL+ALT+C is pressed"""
+	
 	cursor_position = get_position()
 	color = get_color(cursor_position[0], cursor_position[1])
 	hex_color = rgb2hex(color)
+	print("[Color Picked]")
 	print(f"Coordinates: X = {cursor_position[0]}, Y = {cursor_position[1]}")
 	print(f"RGB: {', '.join(str(i) for i in color)}")
 	print(f"HEX: {hex_color}")
 	print()
-	pyperclip.copy(hex_color)
+	pyperclip.copy(hex_color)  # copy the hex code to clipboard.
 
-keyboard.add_hotkey("ctrl+alt+c", on_hotkey_press)
-os.system('cls & title Color Picker & color e')
-print("Point your mouse cursor at anywhere on the screen, hit CTRL + ALT + C")
-print("The HEX color code will be available on your clipboard. (Also printed on the console app)\n")
-keyboard.wait()
+keyboard.add_hotkey(HOTKEY, on_hotkey_press)  # set up the keyboard combo and hook it to the function.
+os.system('cls & title Screen Color Picker & color e')
+print(f"Point your mouse cursor at anywhere on the screen, hit {HOTKEY.upper()}")
+print("The HEX color code will be available on your clipboard. (Also printed here)\n")
+keyboard.wait()  # infinite blocking loop just to listen for the hotkey.
